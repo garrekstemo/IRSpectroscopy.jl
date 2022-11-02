5edd2f378e98d300b0ff1996c75a5884c134f18e
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2022-11-03 07:35:44 +0900

    Refactor: simplify inputs

--- src/input-output.jl
+++ src/input-output.jl
@@ -1,3 +1,6 @@
+"""
+    b_out(ω, ω_0, ω_c, ω_12, γ_1, γ_3, κ, N, A)
+"""
 function b_out(ω, ω_0, ω_c, ω_12, γ_1, γ_3, κ, N, A)
 
     bout = Vector{ComplexF64}(undef, length(ω))
@@ -5,13 +8,15 @@ function b_out(ω, ω_0, ω_c, ω_12, γ_1, γ_3, κ, N, A)
     for (i, ω_i) in enumerate(ω)
         bout_i = -im * 0.5 * κ * (ω_i - ω_0 + im * γ_1) * (ω_i - ω_12 + im * γ_3) / ( (ω_i - ω_c + im * 0.5 * κ) * (ω_i - ω_0 + im * γ_1) * (ω_i - ω_12 + im * γ_3) - N * A[i] )
         bout[i] = bout_i
-        # push!(bout, bout_i) 
     end
 
     return bout
 end
 
-function A(ω, ω_0, g_1, Δ, γ_m, f_pu, B)
+"""
+    A(ω, f_pu, ω_0, Δ, γ_m, g_1, B)
+"""
+function A(ω, f_pu, ω_0, Δ, γ_m, g_1, B)
 
     a = Vector{ComplexF64}(undef, length(ω))
 
@@ -22,20 +27,70 @@ function A(ω, ω_0, g_1, Δ, γ_m, f_pu, B)
     return a
 end
 
-function B(ω, ω_0, g_1, g_3, Δ, γ_m)
+"""
+    B(ω, ω_0, Δ, γ_m, g_1, g_3_ratio)
+
+g_3 = c * g_1, where c = g_3/g_1 can be positive or negative.
+Here we use c.
+"""
+function B(ω, ω_0, Δ, γ_m, g_1, g_3_ratio)
 
     b = Vector{ComplexF64}(undef, length(ω))
 
     for (i, ω_i) in enumerate(ω)
-        b_i = g_3 * (2 * (2 * g_1 + g_3) * (ω_i - ω_0) + (4 * g_1 + g_3) * im * γ_m) - 4 * Δ * g_1^2
+        b_i = g_3 * (2 * (2 * g_1 * (1 + g_3_ratio)) * (ω_i - ω_0) + (4 * g_1 * ( 1 + g_3_ratio)) * im * γ_m) - 4 * Δ * g_1^2
         b[i] = b_i
     end
     b
 end
 
-function calculate_ΔT(ω, ω_0, ω_c, ω_12, g_1, g_3, γ_m, γ_1, γ_3, κ, Δ, N, f_pu)
-    B_ω = B(ω, ω_0, g_1, g_3, Δ, γ_m)
-    A_ω = A(ω, ω_0, g_1, Δ, γ_m, f_pu, B_ω)
+"""
+    transmission(ω, f_pu, ω_0, ω_c, Δ, γ_m, κ, g_1, g_3_ratio, N)
+
+Single-pulse transmission spectrum.
+
+ω = incident wavelength
+f_pu = fraction of molecules excited by pump pulse (0 = no pumping)
+ω_0 = molecular fundamental frequency
+ω_c = cavity mode frequency
+Δ = molecular anharmonicity, Δ = ω_0 - ω_12 (always positive)
+γ_m = molecular mode line width (fwhm)
+κ/2 = cavity line width (but papers seem to maybe just use κ?)
+g_1 = single-molecule light-matter coupling
+g_3 = deviation of vibration 1->2 transition dipole moment μ_12 
+      from the corresponding harmonic oscillator:
+
+      μ_12 = √2 * μ_1 * (1 + g_3 / g_1)
+      
+      (we use just the ratio, g_3 / g_1)
+"""
+function transmission(ω, f_pu, ω_0, ω_c, Δ, γ_m, κ, g_1, g_3_ratio, N)
+
+    ω_12 = ω_0 - 2 * Δ
+    γ_1 = 0.5 * γ_m
+    γ_3 = 0.5 * 3 * γ_m
+
+    B_ω = B(ω, ω_0, Δ, γ_m, g_1, g_3_ratio)
+    A_ω = A(ω, f_pu, ω_0, Δ, γ_m, g_1, B_ω)
+    T = abs2.(b_out(ω, ω_0, ω_c, ω_12, γ_1, γ_3, κ, N, A_ω))
+
+    return T
+end
+
+"""
+    pp_spectrum(ω, f_pu, ω_0, ω_c, Δ, γ_1, κ, g_1, g_3_ratio, N)
+
+Pump-probe spectrum
+    ΔT = T_fpu - T_0
+"""
+function pp_spectrum(ω, f_pu, ω_0, ω_c, Δ, κ, g_1, g_3_ratio, N)
+
+    ω_12 = ω_0 - 2 * Δ
+    γ_1 = 0.5 * γ_m
+    γ_3 = 0.5 * 3 * γ_m
+
+    B_ω = B(ω, ω_0, γ_m, Δ, g_1, g_3_ratio)
+    A_ω = A(ω, f_pu, ω_0, γ_m, Δ, g_1, B_ω)
     T_fpu = abs2.(b_out(ω, ω_0, ω_c, ω_12, γ_1, γ_3, κ, N, A_ω))
 
     A0 = A(ω, ω_0, g_1, Δ, γ_m, 0.0, B_ω)
@@ -43,15 +98,3 @@ function calculate_ΔT(ω, ω_0, ω_c, ω_12, g_1, g_3, γ_m, γ_1, γ_3, κ, Δ
 
     return T_fpu .- T_0
 end
-
-function calculateT(ω, ω_0, ω_c, ω_12, g_1, g_3, γ_m, γ_1, γ_3, κ, Δ, N, f_pu)
-    B_ω = B(ω, ω_0, g_1, g_3, Δ, γ_m)
-    A_ω = A(ω, ω_0, g_1, Δ, γ_m, f_pu, B_ω)
-    T = abs2.(b_out(ω, ω_0, ω_c, ω_12, γ_1, γ_3, κ, N, A_ω))
-
-    return T
-end
-
-# function calculate_ΔT()
-# end
-
