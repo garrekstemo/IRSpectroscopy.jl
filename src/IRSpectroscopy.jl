f87ac4601e51fee86a9cd7d14d7c5dd5294eb0c6
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2022-12-02 08:30:51 +0900

    Remove morse potential

--- src/IRSpectroscopy.jl
+++ src/IRSpectroscopy.jl
@@ -1,12 +1,8 @@
 module IRSpectroscopy
 
 include("input-output.jl")
-include("molecular_potential.jl")
 
 export transmission,
-       pp_spectrum,
-       morse_potential,
-       harmonic,
-       energy_levels
+       pp_spectrum
 
 end
