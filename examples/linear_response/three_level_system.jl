4cf0752e2117eff9a7980c66e60998a5bb53ae1b
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-06-27 17:33:05 +0900

    add three-level system

--- examples/linear_response/three_level_system.jl
+++ examples/linear_response/three_level_system.jl
@@ -2,37 +2,46 @@ using Revise
 using MolecularPolaritonSpectra
 using GLMakie
 
-kB = 1.380649e-23  # Boltzmann constant in J/K
-ħ = 1.0545718e-34  # Reduced Planck constant in J·s
-c = 2.99792458e10  # Speed of light in cm/s
-
-ωs = -5:0.01:5
-
-ω_12 = ω_ph = 1
+ωs = -5:0.01:7
+ω_ph = ω_12 = 1
 ω_23 = 2ω_ph
 κ = 0.1
+κL = κR = 0.5 * κ
 γ = 0.3
 
-μ = 1e8
-temp = 300.0  # temperature in Kelvin
-β = 1 / (kB * temp)  # inverse temperature
 N = 1
-g = 1 #abs(λ_v * μ) * tanh(β * ħ * ω_v / 2)
+g = 1
 
-κL = κR = 1 # 0.5 * κ
+μ_zy = [
+    0 0 0
+    1 0 0
+    1 1 0
+]
+ω_zy = [
+    0    0    0
+    ω_12 0    0
+    3    ω_23 0
+]
+ps = [
+    [1, 1, 1],
+    [0.45, 0.45, 0.1],
+    [0.7, 0.2, 0.1],
+]
 
-susceptibility = χ.(ωs, ω_12, N, g, γ) .+ χ.(ωs, ω_23, N, g, γ)
-T = linear_transmission(ωs, susceptibility, ω_ph, κL, κR, κ)
-# A = linear_absorption.(ωs, ω_ph, ω_12, κL, κ, γ1, N, g)
-# R = 1 .- T .- A
-
-
-fig = Figure(size = (900, 800))
+fig = Figure(size = (600, 1100))
 DataInspector()
 
-ax1 = Axis(fig[1, 1], ylabel = "Intensity")
-lines!(ωs, T, label = "Transmission")
-
-ax2 = Axis(fig[2, 1], xlabel = "Frequency (cm⁻¹)", ylabel = "Intensity")
+axes = [Axis(fig[i, 1], ylabel = "Intensity") for i in eachindex(ps)]
+for i in eachindex(ps)
+    susceptibility = χ(ωs, ps[i], μ_zy, ω_zy, N, γ, 1)
+    T = linear_transmission(ωs, susceptibility, ω_ph, κL, κR, κ)
+    A = ω_ph / κR * imag.(susceptibility) .* T
+    R = 1 .- T .- A
+    lines!(axes[i], ωs, T, label = "T")
+    lines!(axes[i], ωs, A, label = "A")
+    lines!(axes[i], ωs, R, label = "R")
+    
+    axislegend(axes[i], position = :rc)
+end
 
 fig
--- src/linear_response.jl
+++ src/linear_response.jl
@@ -1,6 +1,18 @@
-function χ(ω, ω_v, N, g, γ)
-    -N * abs2(g) / (ω - ω_v + im * 0.5 * γ)
+# function χ(ω, ω_v, N, g, γ)
+#     -N * abs2(g) / (ω - ω_v + im * 0.5 * γ)
+# end
+function χ(ω, p::Vector, μ, ω_zy, N, γ, V_mol)
+    a = zeros(ComplexF64, length(ω))
+    for i in eachindex(ω)
+        for y in 1:length(p)
+            for z in 1:length(p)
+                a[i] += (p[y] - p[z]) * abs2(μ[z, y]) / (ω[i] - ω_zy[z, y] + im * 0.5 * γ)
+            end
+        end
+    end
+    return a .* -N / V_mol
 end
+
 
 """
     linear_transmission(ω, ω_ph, ω_v, N, g, κL, κR, κ, γ)
@@ -14,13 +26,12 @@ g: Coupling strength between the cavity mode and the molecular vibration.
 κR: Right cavity decay rate.
 κ: Total cavity decay rate.
 """
-function linear_transmission(ω, χ, ω_ph, κL, κR, κ)
-    T = similar(ω)
+function linear_transmission(ω, χ::Vector, ω_ph, κL, κR, κ)
+    Ts = similar(ω, Float64)
     for i in eachindex(ω)
-        T_i = κL * κR / abs2((ω[i] - ω_ph + im * 0.5 * κ + χ[i]))
-        T[i] = T_i
+        Ts[i] = κL * κR / abs2((ω[i] - ω_ph + im * 0.5 * κ + χ[i]))
     end
-    return T
+    return Ts
 end
 function linear_transmission(ω, χ, ω_ph, κL, κR, κ)
     κL * κR / abs2((ω - ω_ph + im * 0.5 * κ + χ))
